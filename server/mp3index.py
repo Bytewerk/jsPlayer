231608b3284ef607b608fbb3b772c770e9120a74
Thomas Kolb <thomas@tkolb.de>
2015-07-27 00:06:22 +0200

    mp3index: use python3 and mutagen for tag reading

--- server/mp3index.py
+++ server/mp3index.py
@@ -1,16 +1,17 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # coding: utf-8
 # vim: noexpandtab sw=2 ts=2 sts=2
 
 import os
 import sys
-import pyid3lib
 import json
 import codecs
 import base64
+import mutagen
 
 from PIL import Image
-from StringIO import StringIO
+from io import BytesIO
+from mutagen.mp3 import MP3
 
 THUMBSIZE = (64, 64)
 OUTPUT = "database.json"
@@ -19,20 +20,17 @@ FOLDER = "."
 if len(sys.argv) > 1:
 	FOLDER = sys.argv[1]
 
-def encode_image(obj):
-	mimetype = obj['mimetype']
-	data = obj['data']
-
+def encode_image(data):
 	# load the image
-	im = Image.open(StringIO(data))
+	im = Image.open(BytesIO(data))
 	im.thumbnail(THUMBSIZE, Image.ANTIALIAS)
 
-	output = StringIO()
+	output = BytesIO()
 	im.save(output, format='JPEG')
 	imgdata = output.getvalue()
 	output.close()
 
-	return base64.b64encode(imgdata)
+	return base64.b64encode(imgdata).decode()
 
 mp3db = {}
 
@@ -55,21 +53,33 @@ for root, dirs, files in os.walk(FOLDER):
 
 			print("Scanning {} ...".format(filename))
 
-			id3tags = pyid3lib.tag(filename)
-
 			mp3info = {"/type/": "file"}
-			if hasattr(id3tags, 'artist'):
-				mp3info['artist'] = codecs.decode(id3tags.artist, 'utf-8', 'replace')
-			if hasattr(id3tags, 'title'):
-				mp3info['title'] = codecs.decode(id3tags.title, 'utf-8', 'replace')
-			if hasattr(id3tags, 'album'):
-				mp3info['album'] = codecs.decode(id3tags.album, 'utf-8', 'replace')
-			if 'APIC' in id3tags:
-				print("Image data found.")
-				d = id3tags[id3tags.index('APIC')]
-				mp3info['image'] = encode_image(d)
+
+			try: # to parse the file
+				audio = MP3(filename)
+
+				mp3info['length'] = int(audio.info.length)
+				mp3info['bitrate'] = int(audio.info.bitrate)
+
+				if not audio.tags:
+					print("No tags found.")
+				else:
+					if 'TIT2' in audio.tags:
+						#mp3info['title'] = codecs.decode(audio.tags['TIT2'].text[0], 'utf-8', 'replace')
+						mp3info['title'] = audio.tags['TIT2'].text[0]
+					if 'TALB' in audio.tags:
+						mp3info['album'] = audio.tags['TALB'].text[0]
+					if 'TPE1' in audio.tags:
+						mp3info['artist'] = audio.tags['TPE1'].text[0]
+
+					if 'APIC:' in audio.tags:
+						print("Image data found.")
+						d = audio.tags['APIC:'].data
+						mp3info['image'] = encode_image(d)
+			except mutagen.mp3.HeaderNotFoundError as e:
+				print("FAILED >> {}".format(e))
 
 			dirobj[f] = mp3info
 
 with open(OUTPUT, 'w') as outfile:
-	outfile.write(json.dumps(mp3db, indent=2))
+	outfile.write(json.dumps(mp3db))
